11ab3c19f443f494e998e7306fdf06c4f42074aa
RandyField <287572291@qq.com>
2018-04-13 17:49:41 +0800

    windows service

---
+++ IIS-heartbeat/IIS-heartbeat/Service1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IIS_heartbeat
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(TimedEvent);
+            timer.Interval = 1000 * 10 * 60;//每10分钟执行一次
+            //timer.Interval = 10000;//每19分钟执行一次
+            timer.Enabled = true;
+        }
+
+        //定时执行事件
+        private void TimedEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //Debugger.Launch();
+            Common.HttpHelper httphelper = new Common.HttpHelper();
+
+            //业务逻辑代码
+            //this.WriteLog("iis-heartbeat：【服务业务执行一次】");
+            //Untility.XmlParseHelper.SetPath(false);
+            List<string> list = Untility.XmlParseHelper.GetNodeList("Url", "url");
+            foreach (var url in list)
+            {
+                httphelper.HttpGet(url);
+                this.WriteLog(string.Format("iis-heartbeat：HttpGet-{0}", url));
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            this.WriteLog("iis-heartbeat：【服务启动】");
+
+        }
+
+        protected override void OnStop()
+        {
+            this.WriteLog("iis-heartbeat：【服务停止】");
+        }
+        protected override void OnShutdown()
+        {
+            this.WriteLog("iis-heartbeat：【计算机关闭】");
+        }
+
+        #region 记录日志
+        /// <summary>
+        /// 记录日志
+        /// </summary>
+        /// <param name="msg"></param>
+        private void WriteLog(string msg)
+        {
+
+            //string path = @"C:\log.txt";
+
+            //该日志文件会存在windows服务程序目录下
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\log.txt";
+            FileInfo file = new FileInfo(path);
+            if (!file.Exists)
+            {
+                FileStream fs;
+                fs = File.Create(path);
+                fs.Close();
+            }
+
+            using (FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(DateTime.Now.ToString() + "   " + msg);
+                }
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ IIS-heartbeat/IIS-heartbeat/XmlParseHelper.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Web;
+
+namespace Untility
+{
+    /// <summary>
+    ///  XML解析帮助类
+    /// </summary>
+    public static class XmlParseHelper
+    {
+        /// <summary>
+        /// exe中寻找xml路径
+        /// </summary>
+        public static string xmlPath = AppDomain.CurrentDomain.BaseDirectory + "\\" + ConfigurationManager.AppSettings["xmlRoute"];
+
+      
+
+        /// <summary>
+        /// 静态构造方法
+        /// web中寻找xml路径
+        /// </summary>
+        //static XmlParseHelper()
+        //{
+        //    if (!string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["xmlRoute"]))
+        //    {
+        //        xmlPath = HttpContext.Current.Server.MapPath(ConfigurationManager.AppSettings["xmlRoute"]);              
+        //    }
+        //    else
+        //    {
+        //        xmlPath = HttpContext.Current.Server.MapPath(xmlPath);
+        //    }
+        //}
+
+
+        /// <summary>
+        /// 解析xml节点 获取所有二级节点值
+        /// </summary>
+        /// <param name="firstNodeName">一级节点名</param>
+        /// <param name="secondNodeName">二级节点名</param>
+        /// <returns></returns>
+        public static List<string> GetNodeList(string firstNodeName, string secondNodeName)
+        {
+            List<string> list = null;
+
+            //定义并从xml文件中加载节点（根节点）
+            XElement rootNode = XElement.Load(xmlPath);
+
+            //查询语句：获取根节点下子节点(此时子节点可以跨层次：孙节点、重孙节点)
+            IEnumerable<XElement> targetNodes = from target in rootNode.Descendants(firstNodeName).Descendants(secondNodeName)
+                                                select target;
+            if (targetNodes.Count() > 0)
+            {
+                list = new List<string>();
+                foreach (XElement node in targetNodes)
+                {
+                    list.Add(node.Value);
+                }
+            }
+
+            return list;
+        }
+
+
+        /// <summary>
+        /// 解析xml3级节点下的值
+        /// </summary>
+        /// <param name="firstNodeName"></param>
+        /// <param name="secondNodeName"></param>
+        /// <param name="thirdNodeName"></param>
+        /// <returns></returns>
+        public static List<string> GetNodeList(string firstNodeName, string secondNodeName, string thirdNodeName)
+        {
+            List<string> list = null;
+
+            //定义并从xml文件中加载节点（根节点）
+            XElement rootNode = XElement.Load(xmlPath);
+
+            //查询语句：获取根节点下子节点(此时子节点可以跨层次：孙节点、重孙节点)
+            IEnumerable<XElement> targetNodes = from target in rootNode.Descendants(firstNodeName).Descendants(secondNodeName).Descendants(thirdNodeName)
+                                                select target;
+            if (targetNodes.Count() > 0)
+            {
+                list = new List<string>();
+                foreach (XElement node in targetNodes)
+                {
+                    list.Add(node.Value);
+                }
+            }
+
+            return list;
+        }
+
+
+        /// <summary>
+        /// 解析xml节点 获取一个二级节点值
+        /// </summary>
+        /// <param name="firstNodeName"></param>
+        /// <param name="secondNodeName"></param>
+        /// <returns></returns>
+        public static string GetSingNode(string firstNodeName, string secondNodeName)
+        {
+            string value = "";
+            //定义并从xml文件中加载节点（根节点）
+            XElement rootNode = XElement.Load(xmlPath);
+
+            //查询语句：获取根节点下子节点(此时子节点可以跨层次：孙节点、重孙节点)
+            IEnumerable<XElement> targetNodes = from target in rootNode.Descendants(firstNodeName).Descendants(secondNodeName)
+                                                select target;
+            if (targetNodes.Count() > 0)
+            {
+                XElement node = (XElement)targetNodes.First();
+                value = node.Value;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// 解析xml节点 获取一个1级节点值
+        /// </summary>
+        /// <param name="firstNodeName"></param>
+        /// <returns></returns>
+        public static string GetSingNode(string firstNodeName)
+        {
+            string value = "";
+            //定义并从xml文件中加载节点（根节点）
+            XElement rootNode = XElement.Load(xmlPath);
+
+            //查询语句：获取根节点下子节点(此时子节点可以跨层次：孙节点、重孙节点)
+            IEnumerable<XElement> targetNodes = from target in rootNode.Descendants(firstNodeName)
+                                                select target;
+            if (targetNodes.Count() > 0)
+            {
+                XElement node = (XElement)targetNodes.First();
+                value = node.Value;
+            }
+
+            return value;
+        }
+    }
+}
